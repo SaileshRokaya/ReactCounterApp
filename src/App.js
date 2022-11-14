306fd80034b9976705308416b1b50fcd76fe80e1
SaileshRokaya <79441756+SaileshRokaya@users.noreply.github.com>
2022-11-14 10:58:49 +0545

    Add Style to the Profile Card Component and Make it Dynamic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import MyButton from "./components/MyButton";
-import {useState} from 'react'
+import { useState } from "react";
 import ProfileCard from "./components/ProfileCard";
 // want to make a COUNTER app
 // 1. make a button
@@ -15,16 +15,39 @@ import ProfileCard from "./components/ProfileCard";
 
 // State
 function App() {
-  const [count, setCount] = useState(30)
+  const [count, setCount] = useState(30);
   // const myAge = 20;
   return (
     <>
       <h2 style={{ color: "red" }}>{count}</h2>
-      <button onClick={() => {setCount(count + 1)}}>+</button>
-      <button onClick={() => {setCount(count - 1)}}>-</button>
-      <MyButton title = 'Buy' color= 'red' />
-      <MyButton title = 'Sell' color = 'green' />
-      <ProfileCard/>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        -
+      </button>
+      <MyButton title="Buy" color="red" />
+      <MyButton title="Sell" color="green" />
+      <ProfileCard
+        image="https://scontent.fktm17-1.fna.fbcdn.net/v/t39.30808-6/311454587_1799909447028677_5572289310893600927_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=2uAedg3kY5IAX-2mXn6&_nc_ht=scontent.fktm17-1.fna&oh=00_AfCIgY1OuqHBZdp8xAI3GpopT7CEifVEL1lYsSuLthVxYg&oe=63767F35"
+        name="Sailesh Rokaya"
+        title="CEO & Founder"
+        description="Hello, I'am Sailesh Rokaya origranlly from bansgadhi Bardiya. Now i lives in Kathamndu Nepal."
+      />
+      <ProfileCard
+        image="https://scontent.fktm17-1.fna.fbcdn.net/v/t1.6435-9/168005520_1426768594344964_1311131796353406429_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=174925&_nc_ohc=xO7Cheg5l0cAX9FNUb7&tn=zSla4XmZD0BiTgav&_nc_ht=scontent.fktm17-1.fna&oh=00_AfDZJF_tAUQyuk8D-Oa7sEtOEHqavspZTSw6SPuROeFpcQ&oe=63992A38"
+        name="Ram Kumar Mahato"
+        title="Senior Development"
+        description="Hello, I'am Ram Kumar Mahato origranlly from Birgunj. Now i lives in Kathamndu Nepal."
+      />
     </>
   );
 }
--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,21 +1,22 @@
 import React from "react";
 
-const ProfileCard = () => {
+const ProfileCard = (props) => {
   return (
-    <div style={{ backgroundColor: "white", margin:'10px'}}>
-     <div style={{ display:'flex' }}>
-      <img
-        src="https://scontent.fktm17-1.fna.fbcdn.net/v/t39.30808-6/311454587_1799909447028677_5572289310893600927_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=2uAedg3kY5IAX-2mXn6&_nc_ht=scontent.fktm17-1.fna&oh=00_AfCIgY1OuqHBZdp8xAI3GpopT7CEifVEL1lYsSuLthVxYg&oe=63767F35"
-        alt="ProfileIMG"
-        height="60px"
-      />
-      <div>
-      <h2 style={{marginLeft:'10px'}}>Sailesh Rokaya</h2> 
-      <h5 style={{marginLeft:'10px'}}>Founder & CEO</h5>  
+    <div style={{ backgroundColor: "white", margin: "10px",  borderRadius:10 }}>
+      <div style={{ display: "flex", margin:10, padding:10}}>
+        <img
+          src={props.image}
+          alt="ProfileIMG"
+          height="60px"
+          style={{ borderRadius: 10 }}
+        />
+        <div>
+          <h2 style={{ marginLeft: "10px" }}>{props.name}</h2>
+          <h5 style={{ marginLeft: "10px" }}>{props.title}</h5>
+        </div>
       </div>
-      </div>
       <div>
-          <p style={{fontSize:'10px', padding:'5px'}}>Hello, I'am Sailesh Rokaya origranlly from bansgadhi Bardiya. Now i lives in Kathamndu Nepal.</p>
+        <p style={{ fontSize: "10px", padding: "5px" }}>{props.description}</p>
       </div>
     </div>
   );
