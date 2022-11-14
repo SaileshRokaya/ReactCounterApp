07f39a7aef9902c3125315ee06701a5b79770ef8
SaileshRokaya <79441756+SaileshRokaya@users.noreply.github.com>
2022-11-14 11:26:43 +0545

    Add Fake Database

---
+++ src/fakeDatabase.js
@@ -0,0 +1,28 @@
+const Profiles=[
+    {
+        image:"https://scontent.fktm17-1.fna.fbcdn.net/v/t39.30808-6/311454587_1799909447028677_5572289310893600927_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=2uAedg3kY5IAX-2mXn6&_nc_ht=scontent.fktm17-1.fna&oh=00_AfCIgY1OuqHBZdp8xAI3GpopT7CEifVEL1lYsSuLthVxYg&oe=63767F35",
+        name:"Sailesh Rokaya",
+        title:"CEO & Founder",
+        description:"Hello, I'am Sailesh Rokaya origranlly from bansgadhi Bardiya. Now i lives in Kathamndu Nepal."
+    },
+    {
+        image:'https://scontent.fktm17-1.fna.fbcdn.net/v/t1.6435-9/168005520_1426768594344964_1311131796353406429_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=174925&_nc_ohc=xO7Cheg5l0cAX9FNUb7&tn=zSla4XmZD0BiTgav&_nc_ht=scontent.fktm17-1.fna&oh=00_AfDZJF_tAUQyuk8D-Oa7sEtOEHqavspZTSw6SPuROeFpcQ&oe=63992A38',
+        name:'Ram Kumar Mahato',
+        title:'Senior Development',
+        description:"Hello, I'am Ram Kumar Mahato origranlly from Birgunj. Now i lives in Kathamndu Nepal."
+    },
+    {
+        image:"https://scontent.fktm17-1.fna.fbcdn.net/v/t39.30808-6/311454587_1799909447028677_5572289310893600927_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=2uAedg3kY5IAX-2mXn6&_nc_ht=scontent.fktm17-1.fna&oh=00_AfCIgY1OuqHBZdp8xAI3GpopT7CEifVEL1lYsSuLthVxYg&oe=63767F35",
+        name:"Sailesh Rokaya",
+        title:"CEO & Founder",
+        description:"Hello, I'am Sailesh Rokaya origranlly from bansgadhi Bardiya. Now i lives in Kathamndu Nepal."
+    },
+    {
+        image:'https://scontent.fktm17-1.fna.fbcdn.net/v/t1.6435-9/168005520_1426768594344964_1311131796353406429_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=174925&_nc_ohc=xO7Cheg5l0cAX9FNUb7&tn=zSla4XmZD0BiTgav&_nc_ht=scontent.fktm17-1.fna&oh=00_AfDZJF_tAUQyuk8D-Oa7sEtOEHqavspZTSw6SPuROeFpcQ&oe=63992A38',
+        name:'Ram Kumar Mahato',
+        title:'Senior Development',
+        description:"Hello, I'am Ram Kumar Mahato origranlly from Birgunj. Now i lives in Kathamndu Nepal."
+    },
+]
+
+export default Profiles
